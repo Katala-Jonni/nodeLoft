4e247e48a9e59f9a6f8a1f2c96984c4658f53161
Максим <maksim@iMac-Maksim.local>
2019-02-26 23:16:09 +0300

    home2.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,36 @@
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-const readDir = util.promisify(fs.readdir);
-const fsStat = util.promisify(fs.stat);
-const fsMkDir = util.promisify(fs.mkdir);
-const fsCopyFile = util.promisify(fs.copyFile);
-const fsRmDir = util.promisify(fs.rmdir);
-const deleteFile = require('./deleteFile');
-const readDirIntoInputDir = require('./readDirIntoInputDir');
-const outputFiles = {};
-let pathDirs = [];
-const random = () => Math.floor(Math.random() * 1000);
+const express = require('express');
+const moment = require('moment');
+const app = express();
+const argv = process.argv.slice();
+const valid = param => Number.isFinite(+param);
+const isValid = argv.length > 2 && valid(argv[2]) && valid(argv[3]);
+let time;
+moment.locale('ru');
+const getime = () => moment().format('LL, LTS');
 
-process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
-  process.exit(1);
+process.on('exit', code => {
+  if (code === 1) {
+    console.error('Первый и второй аргументы должны быть цифрами');
+  }
 });
 
-const configArgv = {
-  input: process.argv[2] || 'any-dir',
-  output: process.argv[3] || `dist${random() + random()}`,
-  deleteInput: process.argv[4] || false
+if (!isValid) process.exit(1);
+
+const timer = () => {
+  time = setTimeout(() => {
+    console.log(getime());
+    timer();
+  }, argv[2]);
 };
 
-(async () => {
-  try {
-    const inputDirPath = path.join(process.cwd(), configArgv.input);
-    const stat = await fsStat(inputDirPath);
-    if (stat.isDirectory()) {
-      const files = await readDir(inputDirPath);
-      for await (let file of files) {
-        await readDirIntoInputDir(path.join(inputDirPath, file), outputFiles);
-      }
+app.get('/', (req, res) => {
+  timer();
+  setTimeout(() => {
+    clearTimeout(time);
+    res.send(getime());
+  }, argv[3]);
+});
 
-      const outputDirPath = path.join(process.cwd(), configArgv.output);
-      await fsStat(outputDirPath).catch(() => fsMkDir(outputDirPath));
-
-      for await (const dir of Object.keys(outputFiles)) {
-        const dirPath = path.join(outputDirPath, dir);
-        await fsStat(dirPath).catch(() => fsMkDir(dirPath));
-        if (!fs.existsSync(dirPath)) {
-          fs.mkdirSync(dirPath);
-        }
-        for await (const file of outputFiles[dir]) {
-          const parseFilePath = path.parse(file);
-          await fsCopyFile(file, path.join(dirPath, parseFilePath.base));
-        }
-      }
-
-      if (configArgv.deleteInput) {
-        await deleteFile(inputDirPath, pathDirs);
-        pathDirs.sort((a, b) => b.length - a.length);
-        for await (let dir of pathDirs) {
-          await fsRmDir(dir);
-        }
-        pathDirs = null;
-      }
-    }
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return console.error(`Введите кооректный путь! ${path.parse(e.path).base} не является папкой`);
-    }
-  }
-})();
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App start');
+});
